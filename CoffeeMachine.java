b3fc6ebfd3774343a03975a73d258db2e787a4ed
adamszanto <51482667+adamszanto@users.noreply.github.com>
2020-04-16 18:48:54 +0200

    Update CoffeeMachine.java

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,13 +1,35 @@
 package machine;
+import java.util.Scanner;
+
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need:");
+        int command = scanner.nextInt();
+
+        // Ingredient amount for 1 cup of coffee:
+        // 200 ml of water
+        // 50 ml of milk
+        // 15 g of coffee beans
+        int water = 200;
+        int milk = 50;
+        int coffeeBeans = 15;
+
+        System.out.println("For " + command + " cups of coffee you will need:\n" +
+                command * water + " ml of water\n" +
+                command * milk + " ml of milk\n" +
+                command * coffeeBeans + " g of coffee beans");
+
+
+      /*  System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
         System.out.println("Mixing boiled water with crushed coffee beans");
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        System.out.println("Coffee is ready!"); */
     }
 }
+
+
