f275d72e2c73711ec4a1b8ffc9af929b76846153
adamszanto <51482667+adamszanto@users.noreply.github.com>
2020-04-19 00:10:39 +0200

    Update CoffeeMachine.java

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -6,23 +6,108 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        int water = scanner.nextInt();
+        //    System.out.println("Write how many ml of water the coffee machine has: ");
+        int water = 1200;
 
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = scanner.nextInt();
+      //  System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = 540;
 
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffee = scanner.nextInt();
+      //  System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffee = 120;
 
-        System.out.println("Write how many cups of coffee you will need:");
-        int cups = scanner.nextInt();
+      //  System.out.println("Write how many cups of coffee you will need:");
+        int cups = 9;
+        int money = 550;
+
+        // THIS IS WHERE THE MAIN LOGIC STARTS AFTER THE 5/8 implementation
+
+        System.out.println("The coffee machine has: ");
+        System.out.println(water + " of water");
+        System.out.println(milk + " of milk");
+        System.out.println(coffee + " of coffee beans");
+        System.out.println(cups + " of disposable cups");
+        System.out.println(money + " of money");
+
+        System.out.println("Write action (buy, fill, take):");
+        String command = scanner.nextLine();
+
+        // BUY ACTION LOGIC
+        if(command.equals("buy")){
+            System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - capuccino:");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    water -= 250;
+                    coffee -= 16;
+                    cups -= 1;
+                    money += 4;
+                break;
+
+                case 2:
+                    water -= 350;
+                    milk -= 75;
+                    coffee -= 20;
+                    cups -= 1;
+                    money += 7;
+                break;
+
+                case 3:
+                    water -= 200;
+                    milk -= 100;
+                    coffee -= 12;
+                    cups -= 1;
+                    money += 6;
+
+            }
+
+            System.out.println("The coffee machine has: ");
+            System.out.println(water + " of water");
+            System.out.println(milk + " of milk");
+            System.out.println(coffee + " of coffee beans");
+            System.out.println(cups + " of disposable cups");
+            System.out.println(money + " of money");
+
+        }
+
+        if(command.equals("fill")) {
+            System.out.println("Write how many ml of water do you want to add:");
+            water += scanner.nextInt();
+            System.out.println("Write how many ml of milk do you want to add:");
+            milk += scanner.nextInt();
+            System.out.println("Write how many grams of coffee beans do you want to add:");
+            coffee  += scanner.nextInt();
+            System.out.println("Write how many disposable cups of coffee do you want to add:");
+            cups += scanner.nextInt();
+
+            System.out.println("The coffee machine has: ");
+            System.out.println(water + " of water");
+            System.out.println(milk + " of milk");
+            System.out.println(coffee + " of coffee beans");
+            System.out.println(cups + " of disposable cups");
+            System.out.println(money + " of money");
+        }
+
+        if(command.equals("take")){
+            System.out.println("I gave you $" + money);
+            money = 0;
+
+            System.out.println("The coffee machine has: ");
+            System.out.println(water + " of water");
+            System.out.println(milk + " of milk");
+            System.out.println(coffee + " of coffee beans");
+            System.out.println(cups + " of disposable cups");
+            System.out.println(money + " of money");
+        }
+
+
 
         // The coffee machine needs 200 ml of water, 50 ml of milk and 15 g of coffee beans per one cup of coffee
 
         int waterCounter = water / 200;
         int milkCounter = milk / 50;
         int coffeeCounter = coffee / 15;
+
 
 
         // Find out what is the minimum cup of coffee that we can make with the given amount of ingredient
@@ -43,17 +128,19 @@ public class CoffeeMachine {
             }
         }
 
+        // 4/8 IMPLEMENTATION OF COFFEE MACHINE
+
        // System.out.println("Max amount: " + maxAmount);
 
 
-        if (maxAmount < cups){
+       /* if (maxAmount < cups){
             System.out.println("No, I can make only " + maxAmount + " cup(s) of coffee");
         } else if (maxAmount == cups){
             System.out.println("Yes, I can make that amount of coffee");
         } else if (maxAmount > cups) {
             System.out.println("Yes, I can make that amount of coffee (and even " + (maxAmount - cups) + " more than that");
         }
-
+        */
 
         /*
         // Ingredient amount for 1 cup of coffee:
